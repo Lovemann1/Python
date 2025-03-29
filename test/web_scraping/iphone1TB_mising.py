34ab876baba5586d81f849674a67ab1f5f94e343
Loveman Singh <lovemansingh10@gmail.com>
2025-03-29 17:56:09 +0530

    iPhone_project

---
+++ test/web_scraping/iphone1TB_mising.py
@@ -0,0 +1,31 @@
+#  on 27_03-2025 i removed 'Apple iPhone 14 Pro (256 GB) - Deep Purple (Renewed)' from the filtered list 
+#       because there was no price listed in the website therefor list item was mismatched
+
+from bs4 import BeautifulSoup
+import requests
+url = 'https://www.amazon.ae/s?k=iphone&crid=3Q3DZ0PUU1PWJ&sprefix=%2Caps%2C196&ref=nb_sb_ss_recent_1_0_recent'
+page = requests.get(url)
+soup = BeautifulSoup(page.text,'html')
+all_listitems = soup.find('div', class_= "s-main-slot s-result-list s-search-results sg-row")
+name = all_listitems.find_all('h2')
+clear_name = [_.text for _ in name]
+filtered_list = [item for item in clear_name if item not in ['Results', 'More results','Related searches']]
+    
+print(filtered_list)
+print(len(filtered_list))
+
+
+
+
+#for price
+# price = all_listitems.find_all('span', class_ = 'a-price-whole')
+# clean_price = [_.text for _ in price]
+# print(clean_price)
+# print(len(clean_price))
+
+
+# import pandas as pd
+# from datetime import datetime
+
+# df = pd.DataFrame({'Product Name': filtered_list, 'Price': clean_price, 'date':datetime.today()})
+# print(df)
